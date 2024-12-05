577e93b0f470eb44f45092e74bdeb10786b467c3
Igor <ig.g.like@gmail.com>
2024-12-05 21:55:07 +0600

    :construction_worker:Разделил функционал на модули

---
+++ handlers/picture.py
@@ -0,0 +1,14 @@
+from aiogram import Router, types
+from aiogram.filters import Command
+
+picture_router = Router()
+
+
+@picture_router.message(Command("picture"))
+async def picture_handler(message: types.Message):
+    photo = types.FSInputFile("images/cat.jpg")
+    await message.answer_photo(photo=photo, caption="Кот!")
+
+
+@picture_router.message(Command("pic"))
+async def pic_handler(message: types.Message): ...
