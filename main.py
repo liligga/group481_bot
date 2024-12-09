242200b1fbacf989b0dfa5f78d5d16d6540dd9c1
Igor <igglike@gmail.com>
2024-12-09 16:02:22 +0300

    Сделал диалог и добавил кнопки

--- main.py
+++ main.py
@@ -1,30 +1,23 @@
 import asyncio
-from aiogram import Bot, Dispatcher, types
-from aiogram.filters import Command
-from dotenv import dotenv_values
+import logging
 
-token = dotenv_values(".env")["BOT_TOKEN"]
-bot = Bot(token=token)
-dp = Dispatcher()
-
-
-@dp.message(Command("start"))
-async def start_handler(message: types.Message):
-    name = message.from_user.first_name
-    await message.answer(f"Привет, {name}")
-
-@dp.message()
-async def echo_handler(message: types.Message):
-    txt = message.text
-    await message.answer(txt)
-
-
-
+from bot_config import dp, bot
+from handlers.start import start_router
+from handlers.picture import picture_router
+from handlers.other_messages import echo_router
+from handlers.dialog import opros_router
 
 async def main():
+    dp.include_router(start_router)
+    dp.include_router(picture_router)
+    dp.include_router(opros_router)
+
+    # в самом конце
+    dp.include_router(echo_router)
     # запуск бота
     await dp.start_polling(bot)
 
 if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO)
     asyncio.run(main())
 
